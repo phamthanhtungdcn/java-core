868b630b459874858c68d07f5e9a8a485c9ff1ab
tungpt8158 <tungpt8158@gmail.com>
2025-08-06 20:10:18 +0700

    So sanh hai so

---
+++ src/SoSanhHaiSo.java
@@ -0,0 +1,41 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SoSanhHaiSo {
+    private static double soThuNhat = 0;
+    private static double soThuNhi = 0;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("=== 📐 ỨNG DỤNG SO SANH HAI SO ===");
+
+        while (true){
+            try{
+                System.out.println("Nhap so thu nhat: ");
+                soThuNhat = scanner.nextDouble();
+
+                System.out.println("Nhap so thu nhi: ");
+                soThuNhi = scanner.nextDouble();
+
+                if(soThuNhat != soThuNhi){
+                    System.out.println("Hai so khong bang nhau");
+                } else {
+                    System.out.println("Hai so bang nhau");
+                    break;
+                }
+
+                if(soThuNhat > soThuNhi){
+                    System.out.println("So thu nhat lon hon so thu hai");
+                } else {
+                    System.out.println("So thu nhat nho hon so thu hai");
+                }
+
+                break;
+
+            } catch (InputMismatchException e){
+                System.out.println("❌ Vui long nhap so.");
+                scanner.nextLine();
+            }
+        }
+    }
+}
